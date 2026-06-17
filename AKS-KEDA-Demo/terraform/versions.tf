41255e2ba0c4cef831d848a31abb5d1d09d6a87e
Kasun Rajapakse <kasunsjc@hotmail.com>
2026-06-17 17:57:33 +0200

    fix: upgrade azurerm to ~> 4.0 and azapi to ~> 2.0 to align provider versions across root and module validation

--- AKS-KEDA-Demo/terraform/versions.tf
+++ AKS-KEDA-Demo/terraform/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.116"
+      version = "~> 4.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 1.15"
+      version = "~> 2.0"
     }
     random = {
       source  = "hashicorp/random"
