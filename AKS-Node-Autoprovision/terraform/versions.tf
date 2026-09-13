07cdab080bce18497f6287d3e015dbdf9ef31b22
Kasun Rajapakse <kasunsjc@hotmail.com>
2026-09-13 12:39:45 +0200

    Add resource group features to prevent deletion if it contains resources

--- AKS-Node-Autoprovision/terraform/versions.tf
+++ AKS-Node-Autoprovision/terraform/versions.tf
@@ -11,5 +11,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      # AKS auto-creates a ContainerInsights solution resource in this RG (outside Terraform
+      # state) when the oms_agent add-on is enabled; allow RG deletion despite it.
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
